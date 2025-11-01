1c0b251499ad340f82edbf417de18be76717cffa
Ralf Habacker <ralf.habacker@freenet.de>
2025-11-01 16:29:30 +0100

    bind(): Fix handling zero path length

--- dlls/ws2_32/socket.c
+++ dlls/ws2_32/socket.c
@@ -1222,7 +1222,7 @@ int WINAPI bind( SOCKET s, const struct sockaddr *addr, int len )
             break;
 
         case AF_UNIX:
-            if (len < offsetof(struct sockaddr_un, sun_path))
+            if (len < sizeof(ADDRESS_FAMILY))
             {
                 SetLastError( WSAEFAULT );
                 return -1;
